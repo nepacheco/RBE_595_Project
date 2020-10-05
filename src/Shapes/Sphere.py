8912ab9bec529d071e62921c0995808d37569d12
jdalmeida <37381332+jdalmeida@users.noreply.github.com>
2020-10-05 16:15:34 -0400

    sphere mesh added. fixed imports in Sphere class

--- src/Shapes/Sphere.py
+++ src/Shapes/Sphere.py
@@ -1,9 +1,14 @@
 from .Shape import Shape
-from copy import deepCopy
+from copy import deepcopy
+from src.Shapes.Shape import *
+from math import *
+from src.Grasp import Grasp
+
+
 class Sphere(Shape):
-    def __init__(self):
-        super().__init__()
-        self.radius = 0
+    def __init__(self, radius=0, pose=Pose()):
+        super().__init__(pose)
+        self.radius = radius
 
     # This function was formed assuming
     # Z
@@ -17,7 +22,7 @@ class Sphere(Shape):
         palmDistance = 5
 
         #plan starting pose
-        InitialPose = deepCopy(self.originPose)
+        InitialPose = deepcopy(self.originPose)
         InitialPose.x += self.radius + palmDistance
 
         azimuth = 0
@@ -27,7 +32,7 @@ class Sphere(Shape):
             approachPose = Pose()
             approachPose.x = -1
             radius = self.radius + palmDistance
-            InitialPoseH = deepCopy(self.originPose)
+            InitialPoseH = deepcopy(self.originPose)
             InitialPoseH.x = radius * cos(azimuth*theta)
             InitialPoseH.y = radius * sin(azimuth*theta)
             InitialPoseH.yaw += azimuth*theta
@@ -37,12 +42,12 @@ class Sphere(Shape):
                 thumbPoseH = Pose()
                 thumbPoseH.y = 1
                 thumbPoseH.roll = rotations*phi
-                currentGH=grasp('spherical',InitialPoseH, approachPose, thumbPoseH)
+                currentGH = Grasp('spherical',InitialPoseH, approachPose, thumbPoseH)
                 totalGrasps.append(currentGH)
 
             elevation = 0
             while elevation < divisions:
-                InitialPoseV = deepCopy(InitialPoseH)
+                InitialPoseV = deepcopy(InitialPoseH)
                 InitialPoseV.x = radius * cos(elevation*theta)
                 InitialPoseV.z = radius * sin(elevation*theta)
                 InitialPoseV.pitch += elevation*theta
@@ -51,8 +56,49 @@ class Sphere(Shape):
                     thumbPoseV = Pose()
                     thumbPoseV.y = 1
                     thumbPoseV.roll = rotations * phi
-                    currentGV = grasp('spherical', InitialPoseV, approachPose, thumbPoseV)
+                    currentGV = Grasp('spherical', InitialPoseV, approachPose, thumbPoseV)
                     totalGrasps.append(currentGV)
                 elevation += 1
             azimuth += 1
         return totalGrasps
+
+
+    def makeMesh(self):
+        """
+        creates the mesh grid for a box for the 3d plot by:
+         1) creating the object in a 2D plane at the origin
+         2) extruding by the z to get height
+         3) subtract from z the offset for the centroid
+         3) transforming to the object frame
+        :return: coords [np.array] (x, y, z)
+        """
+
+        # display figure and get axes
+        sphere = mesh.Mesh.from_file('STLs/sphere.STL')
+
+        # Create a new plot
+        figure = plt.figure()
+        axes = mplot3d.Axes3D(figure)
+
+        # move to origin scale points by dimensions
+        vecs = sphere.vectors
+        vecs[:, :, 0] = (vecs[:, :, 0] - 0.5) * self.radius
+        vecs[:, :, 1] = (vecs[:, :, 1] - 0.5) * self.radius
+        vecs[:, :, 2] = (vecs[:, :, 2] - 0.5) * self.radius
+
+        # tranform each point based to based on orientation
+        for f in range(vecs.shape[0]):
+            for v in range(vecs.shape[1]):
+                p = vecs[f, v, :]
+                transformed_p = self.applyTransform(p)
+                vecs[f, v, :] = np.hstack(transformed_p)
+
+        # Load the STL files and add the vectors to the plot
+        axes.add_collection3d(mplot3d.art3d.Poly3DCollection(sphere.vectors, edgecolor='k'))
+
+        # scale plot and add labels
+        scale = sphere.points.flatten()
+        axes.auto_scale_xyz(scale, scale, scale)
+        axes.set_xlabel('X (mm)')
+        axes.set_ylabel('Y (mm)')
+        axes.set_zlabel('Z (mm)')
--- src/main.py
+++ src/main.py
@@ -6,12 +6,13 @@ from stl import mesh
 from mpl_toolkits import mplot3d
 
 from math import pi
-from src.Shapes.Box import *
+from src.Shapes.Box import Box
+from src.Shapes.Sphere import Sphere
 from src.Pose import Pose
 from src.Grasp import Grasp
 
-x = Pose(0, 0, 0, pi/2, pi/6, pi/3)
-box = Box(1, 2, 3, x)
+p0 = Pose(0, 0, 0, pi/2, pi/6, pi/3)
+m = Box(1,3,2, p0)
 
-box.makeMesh()
+m.makeMesh()
 
