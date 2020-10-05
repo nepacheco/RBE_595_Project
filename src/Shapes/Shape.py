e3d1402922867f492aa6783caf38b5a49515747b
jdalmeida <37381332+jdalmeida@users.noreply.github.com>
2020-10-05 15:55:00 -0400

    added primitive stls, with box plotting, scaled and transformed

--- src/Shapes/Shape.py
+++ src/Shapes/Shape.py
@@ -3,14 +3,20 @@
 from src.Pose import Pose
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from stl import mesh
+from mpl_toolkits import mplot3d
 
 
 
 class Shape:
 
     def __init__(self, originPose = Pose()):
-        self.originPose = originPose
+        self.originPose = originPose    # position of centroid of the object with orientation
         self.transformation = Pose.makeTranformfromPose(originPose)
+
+        # params
+        self.ptsPerM = 10
 
     def planGrasps(self, graspParams):
         return
@@ -24,14 +30,20 @@ class Shape:
         :param p: [np.array] (x, y, z) position vector
         :return: [np.array] (x', y', z') transformed position vector
         """
-        vec = np.vstack(np.append(p, [1]))  # add 1 to end and make vertical
+        # make vertical if not already
+        # if p.size[0] != 3:
+        vec = np.vstack(np.append(p, [1]))
+
         newvec = self.transformation.dot(vec)
         return newvec[0:3]
 
-
     def makeMesh(self):
+        """
+        abstract method
+        creates the mesh grid for a shape a box for the 3d plot
+        :return: mesh [x, y, z]
+        """
         return
 
     def visualizeObject(self):
-
         return
--- src/main.py
+++ src/main.py
@@ -1,17 +1,42 @@
 # Script for testing
 
 import numpy as np
-import math
+import matplotlib.pyplot as plt
+from stl import mesh
+from mpl_toolkits import mplot3d
+
 from math import pi
-from src.Shapes.Shape import Shape
+from src.Shapes.Box import *
 from src.Pose import Pose
 from src.Grasp import Grasp
 
-x = Pose(0, 0, 0, pi/2, 0, 0)
-shape = Shape(x)
+x = Pose(0, 0, 0, pi/2, pi/6, pi/3)
+box = Box(1,2,3, x)
 
-print(shape.transformation)
+# mesh = mesh.Mesh.from_file('STLs/cube.STL')
+#
+# # Create a new plot
+# figure = plt.figure()
+# axes = mplot3d.Axes3D(figure)
+#
+# vecs = mesh.vectors
+# length = 3
+# width = 2
+# height = 4
+# numPts = vecs.shape[0]
+# # for pts in range(numPts):
+# vecs[:, :, 0] = vecs[:, :, 0] * length
+# vecs[:, :, 1] = vecs[:, :, 1] * width
+# vecs[:, :, 2] = vecs[:, :, 2] * height
+#
+# # Load the STL files and add the vectors to the plot
+# axes.add_collection3d(mplot3d.art3d.Poly3DCollection(mesh.vectors, edgecolor='k'))
+#
+# scale = mesh.points.flatten()
+# axes.auto_scale_xyz(scale, scale, scale)
+# axes.set_xlabel('X (mm)')
+# axes.set_ylabel('Y (mm)')
+# axes.set_zlabel('Z (mm)')
+box.makeMesh()
+# plt.show()
 
-p = np.array([0, 1, 0])
-s = shape.applyTransform(p)
-print(s)
