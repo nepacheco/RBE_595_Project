0e39e8013787fff25285bd58718a0aae64c98d67
jdalmeida <jfdalmeida@wpi.edu>
2020-10-18 17:00:13 -0400

    plane segmentation and extraction to remove largest plane from cloud

--- pcl_node/src/ShapeFinder.cpp
+++ pcl_node/src/ShapeFinder.cpp
@@ -9,72 +9,69 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl-1.8/pcl/visualization/cloud_viewer.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
-ros::Publisher pub;
+ros::Publisher pubCoeffs;
+ros::Publisher pub_ext_objs;
+
+void extractObjects(pcl::PointCloud<pcl::PointXYZ>::Ptr original_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud);
+
+
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
   // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
-  pcl::PointCloud<pcl::PointXYZ> cloud;
-  pcl::fromROSMsg (*input, cloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromROSMsg (*input, *cloud);
 
-  pcl::ModelCoefficients coefficients;
-  pcl::PointIndices inliers;
+  // Cloud of just the objects (no floor plane)
+  pcl::PointCloud<pcl::PointXYZ>::Ptr obj_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  // Remove floor plane from cloud
+  extractObjects(cloud, obj_cloud);
+
+  // convert clouds to ros msgs for outputting
+  sensor_msgs::PointCloud2 outputCloud;
+  pcl::toROSMsg(*obj_cloud, outputCloud);
+  pub_ext_objs.publish (outputCloud);
+
+  // Publish the model coefficients
+  // pcl_msgs::ModelCoefficients ros_coefficients;
+  // pcl_conversions::fromPCL(*coefficients, ros_coefficients);
+  
+  // pubCoeffs.publish (ros_coefficients);
+}
+
+void extractObjects(pcl::PointCloud<pcl::PointXYZ>::Ptr original_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud)
+{
+  // outputs objects of segmentaion
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+
   // Create the segmentation object
   pcl::SACSegmentation<pcl::PointXYZ> seg;
   // Optional
   seg.setOptimizeCoefficients (true);
-  // Mandatory
+  // Mandatory parameters for segmetation
   seg.setModelType (pcl::SACMODEL_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
   seg.setDistanceThreshold (0.01);
 
-  seg.setInputCloud (cloud.makeShared ());
-  seg.segment (inliers, coefficients);
+  seg.setInputCloud (original_cloud->makeShared ());   // deep copy of input cloud
+  seg.segment (*inliers, *coefficients);      // get largest plane from cloud
 
-  // Publish the model coefficients
-  pcl_msgs::ModelCoefficients ros_coefficients;
-  pcl_conversions::fromPCL(coefficients, ros_coefficients);
-  
-  pub.publish (ros_coefficients);
+  // create extractor
+  pcl::ExtractIndices<pcl::PointXYZ> extract;
+
+  // remove largest plane from cloud
+  extract.setInputCloud (original_cloud);
+  extract.setIndices (inliers);
+  extract.setNegative (true);
+  extract.filter (*filtered_cloud);
 }
-
-// void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
-// {
-//   //callback
-//   // Container for original & filtered data
-//   pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-//   pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-//   pcl::PCLPointCloud2 cloud_filtered;
-
-  
-
-//   // Convert to PCL data type
-//   pcl_conversions::toPCL(*cloud_msg, *cloud);
-
-//   // Convert from PointCloud2 to PointCloud<pcl::PointXYZ>
-//   // pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud;
-//   // pcl::fromPCLPointCloud2(*cloud, *point_cloud);
-
-
-
-//   // Perform the actual filtering
-//   // TODO: MAKE PLANAR FILTERING
-//   pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-//   sor.setInputCloud (cloudPtr);
-//   sor.setLeafSize (0.1, 0.1, 0.1);
-//   sor.filter (cloud_filtered);
-
-//   // Convert to ROS data type
-//   sensor_msgs::PointCloud2 output;
-//   pcl_conversions::moveFromPCL(cloud_filtered, output);
-
-//   // Publish the data
-//   pub.publish (output);
-// }
 
 int main (int argc, char** argv)
 {
@@ -87,7 +84,8 @@ int main (int argc, char** argv)
 
   // Create a ROS publisher for the output point cloud
   // pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
-  pub = nh.advertise<pcl_msgs::ModelCoefficients> ("output", 1);
+  pub_ext_objs = nh.advertise<sensor_msgs::PointCloud2> ("extracted_objects", 1);
+  pubCoeffs = nh.advertise<pcl_msgs::ModelCoefficients> ("output", 1);
 
   // Spin
   ros::spin ();
