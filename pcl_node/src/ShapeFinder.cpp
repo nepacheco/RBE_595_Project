ced1ccc86478ec09127ff95f86e59f4483ecb491
jdalmeida <jfdalmeida@wpi.edu>
2020-10-20 10:36:06 -0400

    added sphere segmentation to pcl_node

--- pcl_node/src/ShapeFinder.cpp
+++ pcl_node/src/ShapeFinder.cpp
@@ -26,9 +26,11 @@
 ros::Publisher pubCoeffs;
 ros::Publisher pub_ext_objs;
 ros::Publisher pub_shape;
+ros::Publisher pub_pose;
 
 void extractObjects(pcl::PointCloud<pcl::PointXYZ>::Ptr original_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud);
 bool segmentCylinder(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud);
+bool segmentSphere(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud);
 void coordsFromCam(float *points, float *coords);
 
 
@@ -44,19 +46,15 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 
   // Remove floor plane from cloud
   extractObjects(cloud, obj_cloud);
-  segmentCylinder(obj_cloud, cylinder_cloud);
+  // segmentCylinder(obj_cloud, cylinder_cloud);
+  segmentSphere(obj_cloud, cylinder_cloud);
 
   // convert clouds to ros msgs for outputting
   sensor_msgs::PointCloud2 outputCloud;
   pcl::toROSMsg(*cylinder_cloud, outputCloud);
   pub_ext_objs.publish (outputCloud);
-
-  // Publish the model coefficients
-  // pcl_msgs::ModelCoefficients ros_coefficients;
-  // pcl_conversions::fromPCL(*coefficients, ros_coefficients);
-  
-  // pubCoeffs.publish (ros_coefficients);
 }
+
 
 bool segmentCylinder(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud){
   
@@ -124,12 +122,12 @@ bool segmentCylinder(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<
   // float centroid[3] = {(avgX + cos(gamma) * radius), (avgY + sin(gamma) * radius), maxH/2};
   coordsFromCam(axis, centroid);
 
-  std::cout << "Radius " << radius
-            << " Height " << maxH
-            << " Centroid " << centroid[0]
-            << "  " << centroid[1]
-            << "  " << centroid[2] <<
-  std::endl;
+  // std::cout << "Radius " << radius
+  //           << " Height " << maxH
+  //           << " Centroid " << centroid[0]
+  //           << "  " << centroid[1]
+  //           << "  " << centroid[2] <<
+  // std::endl;
 
   // Publish the shape message
   shape_node::shapeArray msg;
@@ -137,9 +135,107 @@ bool segmentCylinder(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<
   msg.shapetype.data = "Cylinder";
   msg.pose.position.x = centroid[0];
   msg.pose.position.y = centroid[1];
-  msg.pose.position.z = centroid[2];
+  msg.pose.position.z = centroid[2]/2;
   msg.parameters = {1, 1, 1, 1, maxH, 0, 0, radius};
   pub_shape.publish(msg);
+
+  // Publish pose message for centroid
+  geometry_msgs::PoseStamped pose_msg;
+  pose_msg.pose = msg.pose;
+  pose_msg.header.frame_id = "/map";
+  pub_pose.publish(pose_msg);
+
+  return true;
+}
+
+bool segmentSphere(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud){
+  
+  // Objects needed
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+  pcl::SACSegmentationFromNormals<pcl::PointXYZ, pcl::Normal> seg; 
+  pcl::ExtractIndices<pcl::PointXYZ> extract;
+
+  // Dataset objects
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers_sphere (new pcl::PointIndices);
+
+  // Estimate point normals
+  ne.setSearchMethod (tree);
+  ne.setInputCloud (cloud);
+  ne.setKSearch (50);
+  ne.compute (*cloud_normals);
+
+  // Create the segmentation object for SPHERE segmentation and set all the parameters
+  seg.setOptimizeCoefficients (true);
+  seg.setModelType (pcl::SACMODEL_SPHERE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setNormalDistanceWeight (0.1);
+  seg.setMaxIterations (10000);
+  seg.setDistanceThreshold (0.05);
+  seg.setRadiusLimits (0, 0.2);
+  seg.setInputCloud (cloud);
+  seg.setInputNormals (cloud_normals);
+
+  // Obtain the cylinder inliers and coefficients
+  seg.segment (*inliers_sphere, *coefficients);
+
+  // Extract Cylinder 
+  extract.setInputCloud (cloud);
+  extract.setIndices (inliers_sphere);
+  extract.setNegative (false);
+  extract.filter (*filtered_cloud);
+
+  // Get parameters of cylinder
+  float radius = coefficients->values[3];  
+  float maxH = 0;
+  float axis[3] = {coefficients->values[0], coefficients->values[1], coefficients->values[2]};
+  float centroid[3] = {0, 0, 0};
+
+  // float sumX = 0, sumY = 0, avgX = 0, avgY = 0;
+  // int num_points = filtered_cloud->points.size();
+
+  // for (int i = 0; i < num_points; i++) {
+  //   float points[3] = {filtered_cloud->points[i].x, filtered_cloud->points[i].y, filtered_cloud->points[i].z};
+  //   float coords[3];
+
+  //   coordsFromCam(points, coords);
+  //   if (coords[2] > maxH) maxH = coords[2];
+
+  //   sumX += coords[0];
+  //   sumY += coords[1];
+  // }
+
+  // avgX = sumX / num_points;
+  // avgY = sumY / num_points;
+
+  // float gamma = atan2(avgY, avgX);
+  // float centroid[3] = {(avgX + cos(gamma) * radius), (avgY + sin(gamma) * radius), maxH/2};
+  coordsFromCam(axis, centroid);
+
+  // std::cout << "Radius " << radius
+  //           << " Height " << maxH
+  //           << " Centroid " << centroid[0]
+  //           << "  " << centroid[1]
+  //           << "  " << centroid[2] <<
+  // std::endl;
+
+  // Publish the shape message
+  shape_node::shapeArray msg;
+
+  msg.shapetype.data = "Sphere";
+  msg.pose.position.x = centroid[0];
+  msg.pose.position.y = centroid[1];
+  msg.pose.position.z = centroid[2];
+  msg.parameters = {1, 1, 1, 1, 0, 0, 0, radius};
+  pub_shape.publish(msg);
+
+  // Publish pose message for centroid
+  geometry_msgs::PoseStamped pose_msg;
+  pose_msg.pose = msg.pose;
+  pose_msg.header.frame_id = "/map";
+  pub_pose.publish(pose_msg);
 
   return true;
 }
@@ -196,7 +292,7 @@ void extractObjects(pcl::PointCloud<pcl::PointXYZ>::Ptr original_cloud, pcl::Poi
 int main (int argc, char** argv)
 {
   // Initialize ROS
-  ros::init (argc, argv, "my_pcl_tutorial");
+  ros::init (argc, argv, "pcl_node");
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
@@ -207,6 +303,7 @@ int main (int argc, char** argv)
   pub_ext_objs = nh.advertise<sensor_msgs::PointCloud2> ("extracted_objects", 1);
   pubCoeffs = nh.advertise<pcl_msgs::ModelCoefficients> ("output", 1);
   pub_shape = nh.advertise<shape_node::shapeArray> ("ShapeArray", 1);
+  pub_pose = nh.advertise<geometry_msgs::PoseStamped> ("ShapePose", 1);
 
   // Spin
   ros::spin ();
